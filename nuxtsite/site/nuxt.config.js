4e99b157be13db7fb60d7e174cd4ca39bdff5c3e
jjozwiak <jason.jozwiak@gmail.com>
2018-05-31 13:53:16 +0300

    setting up hugo site

---
+++ nuxtsite/site/nuxt.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'abdtaxaccounting.com',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', integrity: 'sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm', crossorigin: 'anonymous'},
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Oswald:300,400,500,600' },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.0.10/css/all.css', integrity: 'sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg', crossorigin: 'anonymous'}
+    ],
+    script: [
+      { src: 'https://code.jquery.com/jquery-3.2.1.slim.min.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js' },
+      { src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'}
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ nuxtsite/site/pages/services/index.vue
@@ -0,0 +1,158 @@
+<template>
+  <div id="services-content">
+    <header>
+      <div class="lead container">
+        <h1>Services</h1>
+      </div>
+    </header>
+    <section class="mb-5 mt-5">
+      <div class="container">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Ullam quisquam rerum quos adipisci ipsam est labore itaque
+          maxime, doloremque nulla. Numquam provident, alias saepe in
+          sequi aspernatur eos rerum adipisci?</p>
+      </div>
+    </section>
+    <section class="mb-5 mt-5 pb-5 pt-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="~/assets/paper-stack-cropped.jpg" alt="" class="img-fluid">
+          </div>
+          <div class="col-md-6">
+            <h2 id="tax-return-preparation">Tax Return
+              Preparation</h2>
+            <p>Our modern return preparation software allows us to
+              complete a wide variety of returns for you and your
+              business. We also offer electronic filling of
+              federal and most state returns.</p>
+            <ul>
+              <li>Individual</li>
+              <li>Corporations / S-Corps</li>
+              <li>Partnerships</li>
+              <li>LLC/LLP</li>
+              <li>State & Local Returns</li>
+              <li>Quarterly Estimated Tax Payment</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="mb-5 mt-5 pb-5 pt-5 tax-planning">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h2>Tax Planning</h2>
+            <p>Back in 1935 Federal Judge Learned Hand said "Anyone
+              may arrange his affairs so that his taxes shall be
+              as low as possible; he is not bound to choose that
+              pattern which best pays the treasury. There is not
+              even a patriotic duty to increase one's taxes.” A
+              little bit of proactive tax planning is what can
+              help you do just that. ABD Tax Accounting can help
+              you look to the year(s) ahead and help you implement
+              tax saving strategies to maximize your after-tax
+              income.</p>
+            <ul>
+              <li>Buying/Selling a home</li>
+              <li>Getting a divorce</li>
+              <li>Buying/Selling or starting a business</li>
+            </ul>
+          </div>
+          <div class="col-md-6">
+            <img src="~/assets/services-tax-planning.jpg" alt="" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="mb-5 mt-5 pb-5 pt-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="~/assets/services-audit-representation.jpg" alt="" class="img-fluid">
+          </div>
+          <div class="col-md-6">
+            <h2>Tax Audit Assistance and IRS Representation</h2>
+            <p>Facing an audit of your tax return or getting a
+              notice from the IRS can be a stressful experience
+              for anyone, but you don’t have to face them alone.
+              ABD Tax Accounting can help you resolve these
+              matters and get back to a good standing with tax
+              authorities.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    head() {
+      return {
+        title: 'Services | ABD Tax Accounting LLC'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+  @import "~/scss/_variables.scss";
+
+  header {
+    background-image: url('~/assets/about-header-alt-b&w.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 0 -100px;
+    height: 250px;
+    overflow: hidden;
+    position: relative;
+
+    &:before {
+      background: rgba(11, 60, 93, 0.6);
+
+      content: '';
+      height: 250px;
+      left: 0;
+      position: absolute;
+      -webkit-backface-visibility: hidden;
+      width: 100%;
+      z-index: 10;
+    }
+    h1 {
+      color: #FFF;
+    }
+    .lead {
+      bottom: 0;
+      width: 100%;
+      h1 {
+        color: #FFF;
+        font-family: Oswald;
+        font-size: 2.5em;
+        line-height: 1.6em;
+        padding: 80px 0;
+        position: relative;
+        width: 100%;
+        z-index: 100;
+      }
+    }
+  }
+
+  /*img {*/
+    /*height: 300px;*/
+    /*width: 100%;*/
+  /*}*/
+
+  a.service-link {
+    color: inherit;
+    &:hover {
+      color: inherit;
+      text-decoration: none;
+    }
+  }
+
+  .tax-planning {
+    background: $paper;
+  }
+</style>
