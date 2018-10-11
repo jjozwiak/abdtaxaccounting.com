4b28a510342895f17fd77481f48e3a26cf3f78a8
jjozwiak <jason.jozwiak@gmail.com>
2018-10-11 22:48:00 +0300

    adding custom 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <banner-component text="Page Not Found"></banner-component>
+    <div class="container mt-5 mb-5">
+      <h1>Sorry</h1>
+      <p>The page you are looking for does not exist. Please return to the <nuxt-link to="/">Home page</nuxt-link></p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import BannerComponent from '~/components/BannerComponent.vue';
+  export default {
+    components: {
+      BannerComponent
+    },
+    props: ['error'],
+    //layout: 'blog' // you can set a custom layout for the error page
+  }
+</script>
