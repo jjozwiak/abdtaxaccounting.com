702133261f19ffc1d0b970004269e64097174c9d
jjozwiak <jason.jozwiak@gmail.com>
2018-10-12 20:25:58 +0300

    adding thank you page

---
+++ pages/thanks/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="services-content">
+    <banner-component text="Thank You"></banner-component>
+    <section>
+      <div class="container mt-5 mb-5">
+          <p>Your message has been sent. We will get back to you shortly.</p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  import BannerComponent from '~/components/BannerComponent.vue'
+  export default {
+    scrollToTop: true,
+    components: {
+      BannerComponent
+    },
+    head() {
+      return {
+        title: 'Thank You'
+      }
+    }
+  }
+</script>
